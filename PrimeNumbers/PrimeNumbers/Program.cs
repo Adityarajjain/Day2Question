1d78a13e1255e4e5e8e57800d8850fc138074fda
Adityaraj Jain <adityaraj.jain@lntinfotech.com>
2020-12-16 16:35:21 +0530

    CS PrimeNumbers

---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two integers:");
+            int min = int.Parse(Console.ReadLine());
+            int max = int.Parse(Console.ReadLine());
+            for(int i=min;i<=max;i++)
+            {
+                bool flag = false;
+                for(int j=2;j<=(Math.Sqrt(i));j++)
+                {
+                    flag = false;
+                    if (i % j == 0)
+                    { flag = true; break; }
+                }
+                if (!flag)
+                    Console.WriteLine(i + "\t");
+            }
+        }
+    }
+}
